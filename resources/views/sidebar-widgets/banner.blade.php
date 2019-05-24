bf438043cf38e863c4985801d6dc9b9993a56846
vn2015 <vnedogarko@gmail.com>
2019-05-24 14:04:46 +0300

    fixed banners link

--- resources/views/sidebar-widgets/banner.blade.php
+++ resources/views/sidebar-widgets/banner.blade.php
@@ -3,7 +3,9 @@
     <div class="widget-wrapper padding-top-20">
         @foreach($banners as $banner)
             <div class="widget-banner">
-                <img src="{{$banner->file->link}}" alt="banner">
+                <a href="{{$banner->url_redirect}}">
+                    <img src="{{$banner->file->link}}" alt="banner">
+                </a>
             </div>
         @endforeach
     </div>
