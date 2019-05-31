0547e6b86a6f1c9c70ceda02acf3dee36ee68669
vn2015 <vnedogarko@gmail.com>
2019-05-31 14:17:55 +0300

    fixed topic section changing

--- resources/views/admin/forum/topic/edit.blade.php
+++ resources/views/admin/forum/topic/edit.blade.php
@@ -19,7 +19,6 @@
 @endsection
 
 @section('content')
-    {{--{{dd($topic)}}--}}
     <div class="col-md-10 col-md-offset-1">
         <div class="box">
             <div class="box-header with-border">
@@ -34,8 +33,7 @@
                                 <div class="col-md-4">
                                     <div class="box-header">
                                         <h3 class="box-title">Раздел:</h3>
-                                        <!-- /. tools -->
-                                    </div>
+                                    </div> <!-- /.box-header -->
                                     <div class="form-group">
                                         <select class="form-control" name="section_id">
                                             @foreach($sections as $section)
@@ -47,8 +45,7 @@
                                 <div class="col-md-8">
                                     <div class="box-header">
                                         <h3 class="box-title">Название:</h3>
-                                        <!-- /. tools -->
-                                    </div>
+                                    </div><!-- /.box-header -->
                                     <input type="text" name="title" class="form-control" placeholder="Название..." value="{{old('title')??$topic->title}}">
                                     @if ($errors->has('title'))
                                         <span class="invalid-feedback text-red" role="alert">
@@ -61,8 +58,7 @@
                                 <div class="col-md-4">
                                     <div class="box-header">
                                         <h3 class="box-title">Превью:</h3>
-                                        <!-- /. tools -->
-                                    </div>
+                                    </div> <!-- /.box-header -->
                                     <img class="img-responsive" src="{{route('home').($topic->preview_image->link??'/dist/img/default-50x50.gif')}}" alt="Photo">
                                     <br>
                                     <div class="form-group">
@@ -80,9 +76,7 @@
                                         <div>
                                             <div class="box-header">
                                                 <h3 class="box-title">Сокращенное содержание:</h3>
-                                                <!-- /. tools -->
-                                            </div>
-                                            <!-- /.box-header -->
+                                            </div> <!-- /.box-header -->
                                             <div class="box-body pad">
                                         <textarea id="preview_content" name="preview_content" rows="5" cols="80">
                                                                 {!! old('preview_content')??$topic->preview_content !!}
@@ -101,9 +95,8 @@
                                 <div>
                                     <div class="box-header">
                                         <h3 class="box-title">Содержание:</h3>
-                                        <!-- /. tools -->
-                                    </div>
-                                    <!-- /.box-header -->
+                                    </div><!-- /.box-header -->
+
                                     <div class="box-body pad">
                                         <textarea id="content" name="content" rows="10" cols="80">
                                                                 {!! old('content')??$topic->content !!}
@@ -116,29 +109,8 @@
                                     @endif
                                 </div>
                             </div>
-                            <!-- Date -->
                             <div class="row">
                                 <div class="col-md-4">
-                                    <div class="form-group">
-                                        <div class="box-header">
-                                            <h3 class="box-title">Опубликовать с:</h3>
-                                            <!-- /. tools -->
-                                        </div>
-                                        <div class="input-group date">
-                                            <div class="input-group-addon">
-                                                <i class="fa fa-calendar"></i>
-                                            </div>
-                                            <input type="text" name="start_on" class="form-control pull-right" id="datepicker" value="{{old('start_on')??($topic->start_on??'')}}">
-                                            @if ($errors->has('start_on'))
-                                                <span class="invalid-feedback text-red" role="alert">
-                                        <strong>{{ $errors->first('start_on') }}</strong>
-                                    </span>
-                                            @endif
-                                        </div>
-                                        <!-- /.input group -->
-                                    </div>
-                                </div>
-                                <div class="col-md-4 col-md-offset-1">
                                     <br>
                                     <div class="form-group">
                                         <div class="form-group">
@@ -148,8 +120,8 @@
                                             </label>
                                             @if ($errors->has('news'))
                                                 <span class="invalid-feedback text-red" role="alert">
-                                        <strong>{{ $errors->first('news') }}</strong>
-                                    </span>
+                                                    <strong>{{ $errors->first('news') }}</strong>
+                                                </span>
                                             @endif
                                         </div>
                                         <div class="form-group">
@@ -159,14 +131,12 @@
                                             </label>
                                             @if ($errors->has('approved'))
                                                 <span class="invalid-feedback text-red" role="alert">
-                                        <strong>{{ $errors->first('approved') }}</strong>
-                                    </span>
+                                                    <strong>{{ $errors->first('approved') }}</strong>
+                                                </span>
                                             @endif
-                                        </div>
-                                        <!-- /.input group -->
+                                        </div><!-- /.input group -->
                                     </div>
                                 </div>
-
                             </div>
                             <div class="row">
                                 <div class="col-md-1 col-md-offset-11">
@@ -174,9 +144,7 @@
                                     <button type="submit" class="btn btn-primary btn-flat send-message-btn">Сохранить</button>
                                 </div>
                             </div>
-
-                            <!-- /.form group -->
-                        </form>
+                        </form><!-- /.form group -->
                         <br>
                     </div>
                 </div>
