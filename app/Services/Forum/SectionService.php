c001383d7c0999e98a0bcae71ec2edf75fac5d0b
andrey <bezusskii.andrey01@gmail.com>
2019-07-08 23:15:05 +0300

    fix

--- app/Services/Forum/SectionService.php
+++ app/Services/Forum/SectionService.php
@@ -88,7 +88,7 @@ class SectionService
             $time = Carbon::now()->format('Y-m-d');
             $sql = [];
             foreach ($all_sections as $section) {
-                $sql[] = "( select * from `forum_topics` where `approved` = 1 and (`start_on` is null or `start_on` <= '$time') and `section_id` = $section->id ORDER BY updated_at desc limit 5 )";
+                $sql[] = "( select *, GREATEST(updated_at, commented_at) as lastest_date from `forum_topics` where `approved` = 1 and (`start_on` is null or `start_on` <= '$time') and `section_id` = $section->id ORDER BY lastest_date desc limit 5 )";
             }
 
             $sql = implode(" UNION ALL ", $sql);
@@ -129,7 +129,7 @@ class SectionService
      */
     public static function getRecentForums() {
         $time = Carbon::now()->format('Y-m-d');
-        $sql = "( select * from `forum_topics` where `approved` = 1  ORDER BY updated_at desc limit 10 )";
+        $sql = "( select *, GREATEST(updated_at, commented_at) as lastest_date from `forum_topics` where `approved` = 1  ORDER BY lastest_date desc limit 10 )";
         $recent_forums = collect(\DB::select($sql));
 
         return $recent_forums;
