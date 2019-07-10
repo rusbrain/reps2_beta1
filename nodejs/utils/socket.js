39ecc2a3cbf1a381d923fd4ccf8b3d652ccd52ba
andrey <bezusskii.andrey01@gmail.com>
2019-07-10 17:51:05 +0300

    fix

--- nodejs/utils/socket.js
+++ nodejs/utils/socket.js
@@ -37,28 +37,6 @@ class Socket {
                 this.io.emit('addMessageResponse', result);
             });
 
-            socket.on('typing', function (data) {
-                socket.to(data.socket_id).emit('typing', { typing: data.typing, to_socket_id: socket.id });
-            });
-
-            socket.on('upload-image', async (response) => {
-                let dir = moment().format("D-M-Y") + "/" + moment().format('x') + "/" + response.fromUserId
-                await helper.mkdirSyncRecursive(dir);
-                let filepath = dir + "/" + response.fileName;
-                var writer = fs.createWriteStream(path.basename('uploads') + "/" + filepath, { encoding: 'base64' });
-                writer.write(response.message);
-                writer.end();
-                writer.on('finish', function () {
-                    response.message = response.fileName;
-                    response.filePath = filepath;
-                    response.date = new moment().format("Y-MM-D");
-                    response.time = new moment().format("hh:mm A");
-                    this.insertMessage(response, socket);
-                    socket.to(response.toSocketId).emit('addMessageResponse', response);
-                    socket.emit('image-uploaded', response);
-                }.bind(this));
-            });
-
             socket.on('disconnect', async () => {
                
             });
@@ -68,15 +46,7 @@ class Socket {
  
     socketConfig() {
         this.io.use(async (socket, next) => {            
-            next();
-            // let userId = socket.request._query['id'];
-            // let userSocketId = socket.id;
-            // const response = await helper.addSocketId({user_id:userId, socket_id:userSocketId});
-            // if(response &&  response !== null){
-            //     next();
-            // }else{
-            //     console.error(`Socket connection failed, for  user Id ${userId}.`);
-            // }
+            next();           
         });
         this.socketEvents();
     }
