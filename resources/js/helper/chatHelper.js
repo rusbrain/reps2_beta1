d8279bb4cd34d1eb5cd9308b9f88f1f4078c7e3c
andrey <bezusskii.andrey01@gmail.com>
2019-07-22 11:39:47 +0300

    fix

--- resources/js/helper/chatHelper.js
+++ resources/js/helper/chatHelper.js
@@ -1,30 +1,37 @@
-
-export const getSelection = () => {
-    var txtarea = document.getElementById("editor");
-    var start = txtarea.selectionStart;
-    var finish = txtarea.selectionEnd;
-    var sel = txtarea.value.substring(start, finish);
+export const textareaObj = () => {
+    return document.getElementById("editor");
+};
+export const getSelection = () => {    
+    var start = textareaObj().selectionStart;
+    var finish = textareaObj().selectionEnd;
+    var sel = textareaObj().value.substring(start, finish);
     return sel;
 };
 export const bold = () => {
     let sel = getSelection();
     if (sel.length > 0) {
-      let newValue = document.getElementById("editor").value.replace(sel, '[b]'+sel+'[/b]');
-      document.getElementById("editor").value = newValue;
+      let newValue = textareaObj().value.replace(sel, '[b]'+sel+'[/b]');
+      textareaObj().value = newValue;
+    } else {
+        insertAtCaret('[b][/b]')
     }
 };
 export const italic = () => {
     let sel = getSelection();
     if (sel.length > 0) {
-      let newValue = document.getElementById("editor").value.replace(sel, '[i]'+sel+'[/i]');
-      document.getElementById("editor").value = newValue;
+      let newValue = textareaObj().value.replace(sel, '[i]'+sel+'[/i]');
+      textareaObj().value = newValue;
+    }  else {
+        insertAtCaret('[i][/i]')
     }
 };
 export const underline = () => {
     let sel = getSelection();
     if (sel.length > 0) {
-      let newValue = document.getElementById("editor").value.replace(sel, '[u]'+sel+'[/u]');
-      document.getElementById("editor").value = newValue;
+      let newValue = textareaObj().value.replace(sel, '[u]'+sel+'[/u]');
+      textareaObj().value = newValue;
+    } else {
+        insertAtCaret('[u][/u]')
     }
 };
 export const atmark = () => {
@@ -46,3 +53,42 @@ export const fontColor = () => {
 export const fontSize = () => {
 
 };
+
+export const insertAtCaret = (text) => {
+    var txtarea = textareaObj();
+    if (!txtarea) {
+      return;
+    }
+  
+    var scrollPos = txtarea.scrollTop;
+    var strPos = 0;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ?
+      "ff" : (document.selection ? "ie" : false));
+    if (br == "ie") {
+      txtarea.focus();
+      var range = document.selection.createRange();
+      range.moveStart('character', -txtarea.value.length);
+      strPos = range.text.length;
+    } else if (br == "ff") {
+      strPos = txtarea.selectionStart;
+    }
+  
+    var front = (txtarea.value).substring(0, strPos);
+    var back = (txtarea.value).substring(strPos, txtarea.value.length);
+    txtarea.value = front + text + back;
+    strPos = strPos + text.length;
+    if (br == "ie") {
+      txtarea.focus();
+      var ieRange = document.selection.createRange();
+      ieRange.moveStart('character', -txtarea.value.length);
+      ieRange.moveStart('character', strPos);
+      ieRange.moveEnd('character', 0);
+      ieRange.select();
+    } else if (br == "ff") {
+      txtarea.selectionStart = strPos;
+      txtarea.selectionEnd = strPos;
+      txtarea.focus();
+    }
+  
+    txtarea.scrollTop = scrollPos;
+  }
