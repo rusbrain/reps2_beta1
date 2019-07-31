d1f82b0b7b1c666ae19487c10265d183803f0aef
andrey <bezusskii.andrey01@gmail.com>
2019-07-31 19:29:03 +0300

    fixed admin middle ware for moderators

--- app/Http/Middleware/AdminPanel.php
+++ app/Http/Middleware/AdminPanel.php
@@ -15,10 +15,25 @@ class AdminPanel
      * @return mixed
      */
     public function handle($request, Closure $next)
-    {
-        if (Auth::user()->user_role_id != 1 && Auth::user()->user_role_id != 2) {
-        return redirect('/');
-    }
+    {       
+        if (Auth::user()->user_role_id == 0) {
+            return redirect('/');
+        }
+
+        // Normal admins
+        if (Auth::user()->user_role_id == 2) {
+            //prevent admin.user.role and admin.dbbackup for Normal users
+            if($request->is("admin_panel/user/role") || $request->is("admin_panel/dbbackup/*")) {
+                return redirect('admin_panel/');
+            }
+        }
+
+        // Morderators
+        if (Auth::user()->user_role_id == 3) {
+            if(!$request->is("admin_panel/forum/*") && !$request->is("admin_panel/replay/*") && !$request->is("admin_panel")) {
+                return redirect('admin_panel/');
+            }
+        }        
 
         return $next($request);
     }
