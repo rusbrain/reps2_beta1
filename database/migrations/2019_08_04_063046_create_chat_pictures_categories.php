af140034aceb816b4f4d5e47f793ba0bb605ebf8
andrey <bezusskii.andrey01@gmail.com>
2019-08-04 09:49:31 +0300

    new migrate for chat picture category

---
+++ database/migrations/2019_08_04_063046_create_chat_pictures_categories.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChatPicturesCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_pictures_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_pictures_categories');
+    }
+}
