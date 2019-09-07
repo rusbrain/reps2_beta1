f3bd2b68080676c47f65c6ff61301a94afc29698
nl169 <NikolaiL@airmail.cc>
2019-09-07 21:20:53 +0300

    User's replay upload MIME update

--- app/Http/Controllers/ReplayController.php
+++ app/Http/Controllers/ReplayController.php
@@ -316,9 +316,14 @@ class ReplayController extends Controller
         /* @var UploadedFile $file */
 
         $acceptedSafeMimes = [
+            'application/x-compressed',
             'application/x-7z-compressed',
             'application/zip',
-            'application/x-rar-compressed'
+            'application/x-zip-compressed',
+            'application/zip',
+            'multipart/x-zip',
+            'application/x-rar-compressed',
+            'application/x-rar'
         ];
 
         if (in_array($file->getMimeType(), $acceptedSafeMimes)) {
